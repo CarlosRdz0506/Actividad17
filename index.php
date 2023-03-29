b53eeae48553ef8adb6a3a74e94145217a4ef7d6
itielrodriguez <>
2023-03-29 14:12:49 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<html>
+  <head>
+    <title>Datos</title>
+  </head>
+  <body>
+    <?php
+       $nombre = "Carlos Itiel Rdz Amador";
+       $calle = "Joachin Grande";
+       $numero = "653";
+       $colonia = "Nueva Tlaxcala";
+       $municipio = "Saltillo Coahuila";
+       $telefono = "844 893 678";
+
+       echo "¿Cual es tu nombre?: $nombre  ";
+        ?>
+<br>
+<?php
+       echo "¿En que calle vives?: $calle ";
+        ?>
+<br>
+<?php
+       echo "¿En que colonia vives?: $colonia ";
+        ?>
+<br>
+<?php
+       echo " El numero es: $numero ";
+        ?>
+<br>
+<?php
+       echo " Es en el municipio de: $municipio  ";
+       ?>
+<br>
+<?php
+       echo " Si es que te pierdes llamame y te doy indicaciones:$telefono";
+    ?>
+  </body>
+</html>
